c59ffa2072eacede328444094655607f54c9b263
tommyli3318 <tommyli3318@gmail.com>
2020-12-01 20:59:59 -0800

    minor fix

--- app-def/param-stack.ts
+++ app-def/param-stack.ts
@@ -18,7 +18,7 @@ export abstract class ParametersStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         this.Parameters = new ParamUtil(scope, id, props);
-        this.nameSpace = this.node.tryGetContext('param-namespace');
+        this.nameSpace = this.node.tryGetContext('stack-namespace');
         this.DeployEnv = this.node.tryGetContext('deployment-env');
         this.construct();
     }
